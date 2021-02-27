3b3ef2817cc6cb489e48c94cdb7b0ff4eef8d158
Benjamin Arana-Sanchez <benjamin.aranasanchez@bristol.ac.uk>
2021-02-27 00:49:01 +0000

    Added Solutions for weeks 1 and 3

---
+++ Solutions/week3/cube.py
@@ -0,0 +1,10 @@
+# cube.py
+
+def cube(x):
+    """Returns the cube of x"""
+    return x**3  # <--- This obviously doesn't work correctly
+
+def test_square():
+    assert cube(0) == 0
+    assert cube(2) == 8
+    assert cube(-2) == 8
